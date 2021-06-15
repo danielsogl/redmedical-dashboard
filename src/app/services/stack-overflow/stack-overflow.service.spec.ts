240d6afa0b581464fa7cba3004f07fcd7262547f
Daniel Sogl <daniel.sogl@wipro.com>
2021-06-15 22:44:15 +0200

    refactor(app): test stack overflow service result mapping

--- src/app/services/stack-overflow/stack-overflow.service.spec.ts
+++ src/app/services/stack-overflow/stack-overflow.service.spec.ts
@@ -12,11 +12,65 @@ describe('HttpClient testing', () => {
 
   beforeEach(() => (spectator = createHttp()));
 
-  it('should get results by keyword', () => {
-    spectator.service.searchByKeyword('foo').subscribe();
-    spectator.expectOne(
+  it('should get results by keyword', (done) => {
+    spectator.service.searchByKeyword('foo').subscribe((result) => {
+      expect(result).toEqual([
+        {
+          tags: ['javascript', 'typescript'],
+          owner: {
+            reputation: 18999,
+            user_id: 599184,
+            user_type: 'registered',
+            accept_rate: 78,
+            profile_image: 'https://i.stack.imgur.com/tjsXC.jpg?s=128&g=1',
+            display_name: 'Leo Jiang',
+            link: 'https://stackoverflow.com/users/599184/leo-jiang',
+          },
+          is_answered: false,
+          view_count: 24,
+          answer_count: 0,
+          score: 0,
+          last_activity_date: 1623721436,
+          creation_date: 1623721436,
+          question_id: 67978990,
+          content_license: 'CC BY-SA 4.0',
+          link: 'https://stackoverflow.com/questions/67978990/typescript-static-class-properties-that-reference-the-subclasss-properties',
+          title:
+            'Typescript: static class properties that reference the subclass&#39;s properties?',
+        },
+      ]);
+      done();
+    });
+    const req = spectator.expectOne(
       'https://api.stackexchange.com/2.2/search?pagesize=20&order=desc&sort=activity&site=stackoverflow&intitle=foo',
       HttpMethod.GET
     );
+    req.flush({
+      items: [
+        {
+          tags: ['javascript', 'typescript'],
+          owner: {
+            reputation: 18999,
+            user_id: 599184,
+            user_type: 'registered',
+            accept_rate: 78,
+            profile_image: 'https://i.stack.imgur.com/tjsXC.jpg?s=128&g=1',
+            display_name: 'Leo Jiang',
+            link: 'https://stackoverflow.com/users/599184/leo-jiang',
+          },
+          is_answered: false,
+          view_count: 24,
+          answer_count: 0,
+          score: 0,
+          last_activity_date: 1623721436,
+          creation_date: 1623721436,
+          question_id: 67978990,
+          content_license: 'CC BY-SA 4.0',
+          link: 'https://stackoverflow.com/questions/67978990/typescript-static-class-properties-that-reference-the-subclasss-properties',
+          title:
+            'Typescript: static class properties that reference the subclass&#39;s properties?',
+        },
+      ],
+    });
   });
 });
