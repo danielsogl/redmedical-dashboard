5be1fbd90102cd22ad63fe3dc52efbcb9f65e60b
Daniel Sogl <daniel.sogl@wipro.com>
2021-06-16 13:44:22 +0200

    tests(e2e): use fixtures to avoid quote limits

--- cypress/integration/pages/dashboard.spec.ts
+++ cypress/integration/pages/dashboard.spec.ts
@@ -1,11 +1,21 @@
 describe('My First Test', () => {
   beforeEach(() => {
+    cy.intercept('https://api.stackexchange.com/2.2/*', {
+      fixture: 'stack-overflow.json',
+    }).as('fetchStackData');
+
+    cy.intercept('assets/data/weather.json', {
+      fixture: 'weather.json',
+    }).as('weatherData');
+
     cy.visit('/dashboard');
+    cy.wait('@fetchStackData');
+    cy.wait('@weatherData');
   });
 
   describe('test angular widget', () => {
-    it('should render angular widget container', () => {
-      cy.get('.mat-card-header').eq(0).contains('Angular');
+    it('should render widget container', () => {
+      cy.get('mat-card').eq(0).children('.mat-card-header').contains('Angular');
     });
 
     it('should render widget items', () => {
@@ -37,8 +47,11 @@ describe('My First Test', () => {
   });
 
   describe('test typescript widget', () => {
-    it('should render angular widget container', () => {
-      cy.get('.mat-card-header').eq(2).contains('TypeScript');
+    it('should render widget container', () => {
+      cy.get('mat-card')
+        .eq(2)
+        .children('.mat-card-header')
+        .contains('TypeScript');
     });
 
     it('should render widget items', () => {
@@ -70,32 +83,12 @@ describe('My First Test', () => {
   });
 
   describe('test weather widget', () => {
-    it('should render angular widget container', () => {
-      cy.get('.mat-card-header').eq(1).contains('Weather');
+    it('should render widget container', () => {
+      cy.get('mat-card').eq(1).children('.mat-card-header').contains('Weather');
     });
 
     it('should render widget items', () => {
       cy.get('.mat-list-item', { timeout: 10000 }).eq(1);
-    });
-
-    it('should render stack and weather data one by one', () => {
-      cy.get('.mat-list-item', { timeout: 10000 })
-        .eq(1)
-        .get('app-stack-overflow-content')
-        .eq(0)
-        .contains('Answerers');
-
-      cy.get('.mat-list-item', { timeout: 10000 })
-        .eq(1)
-        .get('app-weather-content')
-        .eq(1)
-        .contains('Weather Forecast');
-
-      cy.get('.mat-list-item', { timeout: 10000 })
-        .eq(1)
-        .get('app-stack-overflow-content')
-        .eq(1)
-        .contains('Answerers');
     });
 
     it('should render no data text', () => {
